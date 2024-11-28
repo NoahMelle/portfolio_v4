2305e90e7ee9d83d54d08493c348304e8e275323
= <noahmkamphuisen@gmail.com>
2024-11-28 19:45:28 +0100

    Added security headers

--- main/next.config.ts
+++ main/next.config.ts
@@ -18,6 +18,36 @@ const nextConfig = {
     sassOptions: {
         silenceDeprecations: ["legacy-js-api"],
     },
+
+    async headers() {
+        return [
+            {
+                source: "/(.*)",
+                headers: [
+                    {
+                        key: "Strict-Transport-Security",
+                        value: "max-age=31536000; includeSubDomains; preload",
+                    },
+                    {
+                        key: "X-Frame-Options",
+                        value: "SAMEORIGIN",
+                    },
+                    {
+                        key: "X-Content-Type-Options",
+                        value: "nosniff",
+                    },
+                    {
+                        key: "Referrer-Policy",
+                        value: "same-origin",
+                    },
+                    {
+                        key: "Permissions-Policy",
+                        value: "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()",
+                    }
+                ],
+            },
+        ];
+    },
 };
 
 export default withNextIntl(nextConfig);
