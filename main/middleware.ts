b97e7550774d361244ee6b9239b708ad8341212b
NoahMelle <noahmkamphuisen@gmail.com>
2025-04-16 18:31:45 +0100

    Change favicon name

--- main/middleware.ts
+++ main/middleware.ts
@@ -6,6 +6,6 @@ export default createMiddleware(routing);
 export const config = {
   // Match only internationalized pathnames
   matcher: [
-    "/((?!api|_next/static|_next/image|img|favicon.ico|apple-touch-icon.png|favicon.svg|images/books|icons|manifest).*)",
+    "/((?!api|_next/static|_next/image|img|icon.ico|favicon.ico|apple-touch-icon.png|favicon.svg|images/books|icons|manifest).*)",
   ],
 };
